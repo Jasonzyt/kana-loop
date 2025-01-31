9e81bf2874106c5df157fd628efd84b5f23c6d8e
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-02-01 00:38:27 +0800

    feat: add lists & basic algo

---
+++ src/utils/gojuon.ts
@@ -0,0 +1,572 @@
+export const SEION_LIST = [
+  // 清音
+  {
+    hira: "あ",
+    kana: "ア",
+    romaji: "a",
+  },
+  {
+    hira: "い",
+    kana: "イ",
+    romaji: "i",
+  },
+  {
+    hira: "う",
+    kana: "ウ",
+    romaji: "u",
+  },
+  {
+    hira: "え",
+    kana: "エ",
+    romaji: "e",
+  },
+  {
+    hira: "お",
+    kana: "オ",
+    romaji: "o",
+  },
+  {
+    hira: "か",
+    kana: "カ",
+    romaji: "ka",
+  },
+  {
+    hira: "き",
+    kana: "キ",
+    romaji: "ki",
+  },
+  {
+    hira: "く",
+    kana: "ク",
+    romaji: "ku",
+  },
+  {
+    hira: "け",
+    kana: "ケ",
+    romaji: "ke",
+  },
+  {
+    hira: "こ",
+    kana: "コ",
+    romaji: "ko",
+  },
+  {
+    hira: "さ",
+    kana: "サ",
+    romaji: "sa",
+  },
+  {
+    hira: "し",
+    kana: "シ",
+    romaji: "shi",
+  },
+  {
+    hira: "す",
+    kana: "ス",
+    romaji: "su",
+  },
+  {
+    hira: "せ",
+    kana: "セ",
+    romaji: "se",
+  },
+  {
+    hira: "そ",
+    kana: "ソ",
+    romaji: "so",
+  },
+  {
+    hira: "た",
+    kana: "タ",
+    romaji: "ta",
+  },
+  {
+    hira: "ち",
+    kana: "チ",
+    romaji: "chi",
+  },
+  {
+    hira: "つ",
+    kana: "ツ",
+    romaji: "tsu",
+  },
+  {
+    hira: "て",
+    kana: "テ",
+    romaji: "te",
+  },
+  {
+    hira: "と",
+    kana: "ト",
+    romaji: "to",
+  },
+  {
+    hira: "な",
+    kana: "ナ",
+    romaji: "na",
+  },
+  {
+    hira: "に",
+    kana: "ニ",
+    romaji: "ni",
+  },
+  {
+    hira: "ぬ",
+    kana: "ヌ",
+    romaji: "nu",
+  },
+  {
+    hira: "ね",
+    kana: "ネ",
+    romaji: "ne",
+  },
+  {
+    hira: "の",
+    kana: "ノ",
+    romaji: "no",
+  },
+  {
+    hira: "は",
+    kana: "ハ",
+    romaji: "ha",
+  },
+  {
+    hira: "ひ",
+    kana: "ヒ",
+    romaji: "hi",
+  },
+  {
+    hira: "ふ",
+    kana: "フ",
+    romaji: "fu",
+  },
+  {
+    hira: "へ",
+    kana: "ヘ",
+    romaji: "he",
+  },
+  {
+    hira: "ほ",
+    kana: "ホ",
+    romaji: "ho",
+  },
+  {
+    hira: "ま",
+    kana: "マ",
+    romaji: "ma",
+  },
+  {
+    hira: "み",
+    kana: "ミ",
+    romaji: "mi",
+  },
+  {
+    hira: "む",
+    kana: "ム",
+    romaji: "mu",
+  },
+  {
+    hira: "め",
+    kana: "メ",
+    romaji: "me",
+  },
+  {
+    hira: "も",
+    kana: "モ",
+    romaji: "mo",
+  },
+  {
+    hira: "や",
+    kana: "ヤ",
+    romaji: "ya",
+  },
+  {
+    hira: "ゆ",
+    kana: "ユ",
+    romaji: "yu",
+  },
+  {
+    hira: "よ",
+    kana: "ヨ",
+    romaji: "yo",
+  },
+  {
+    hira: "ら",
+    kana: "ラ",
+    romaji: "ra",
+  },
+  {
+    hira: "り",
+    kana: "リ",
+    romaji: "ri",
+  },
+  {
+    hira: "る",
+    kana: "ル",
+    romaji: "ru",
+  },
+  {
+    hira: "れ",
+    kana: "レ",
+    romaji: "re",
+  },
+  {
+    hira: "ろ",
+    kana: "ロ",
+    romaji: "ro",
+  },
+  {
+    hira: "わ",
+    kana: "ワ",
+    romaji: "wa",
+  },
+  {
+    hira: "を",
+    kana: "ヲ",
+    romaji: "wo",
+  },
+  {
+    hira: "ん",
+    kana: "ン",
+    romaji: "n",
+  },
+];
+
+export const DAKUON_LIST = [
+  // 浊音
+  {
+    hira: "が",
+    kana: "ガ",
+    romaji: "ga",
+  },
+  {
+    hira: "ぎ",
+    kana: "ギ",
+    romaji: "gi",
+  },
+  {
+    hira: "ぐ",
+    kana: "グ",
+    romaji: "gu",
+  },
+  {
+    hira: "げ",
+    kana: "ゲ",
+    romaji: "ge",
+  },
+  {
+    hira: "ご",
+    kana: "ゴ",
+    romaji: "go",
+  },
+  {
+    hira: "ざ",
+    kana: "ザ",
+    romaji: "za",
+  },
+  {
+    hira: "じ",
+    kana: "ジ",
+    romaji: "ji",
+  },
+  {
+    hira: "ず",
+    kana: "ズ",
+    romaji: "zu",
+  },
+  {
+    hira: "ぜ",
+    kana: "ゼ",
+    romaji: "ze",
+  },
+  {
+    hira: "ぞ",
+    kana: "ゾ",
+    romaji: "zo",
+  },
+  {
+    hira: "だ",
+    kana: "ダ",
+    romaji: "da",
+  },
+  {
+    hira: "ぢ",
+    kana: "ヂ",
+    romaji: "ji", // This is pronounced the same as "じ" in modern Japanese
+  },
+  {
+    hira: "づ",
+    kana: "ヅ",
+    romaji: "zu", // This is pronounced the same as "ず"
+  },
+  {
+    hira: "で",
+    kana: "デ",
+    romaji: "de",
+  },
+  {
+    hira: "ど",
+    kana: "ド",
+    romaji: "do",
+  },
+  {
+    hira: "ば",
+    kana: "バ",
+    romaji: "ba",
+  },
+  {
+    hira: "び",
+    kana: "ビ",
+    romaji: "bi",
+  },
+  {
+    hira: "ぶ",
+    kana: "ブ",
+    romaji: "bu",
+  },
+  {
+    hira: "べ",
+    kana: "ベ",
+    romaji: "be",
+  },
+  {
+    hira: "ぼ",
+    kana: "ボ",
+    romaji: "bo",
+  },
+  {
+    hira: "ぱ",
+    kana: "パ",
+    romaji: "pa",
+  },
+  {
+    hira: "ぴ",
+    kana: "ピ",
+    romaji: "pi",
+  },
+  {
+    hira: "ぷ",
+    kana: "プ",
+    romaji: "pu",
+  },
+  {
+    hira: "ぺ",
+    kana: "ペ",
+    romaji: "pe",
+  },
+  {
+    hira: "ぽ",
+    kana: "ポ",
+    romaji: "po",
+  },
+];
+
+export const YOUON_LIST = [
+  // 拗音
+  {
+    hira: "きゃ",
+    kana: "キャ",
+    romaji: "kya",
+  },
+  {
+    hira: "きゅ",
+    kana: "キュ",
+    romaji: "kyu",
+  },
+  {
+    hira: "きょ",
+    kana: "キョ",
+    romaji: "kyo",
+  },
+  {
+    hira: "しゃ",
+    kana: "シャ",
+    romaji: "sha",
+  },
+  {
+    hira: "しゅ",
+    kana: "シュ",
+    romaji: "shu",
+  },
+  {
+    hira: "しょ",
+    kana: "ショ",
+    romaji: "sho",
+  },
+  {
+    hira: "ちゃ",
+    kana: "チャ",
+    romaji: "cha",
+  },
+  {
+    hira: "ちゅ",
+    kana: "チュ",
+    romaji: "chu",
+  },
+  {
+    hira: "ちょ",
+    kana: "チョ",
+    romaji: "cho",
+  },
+  {
+    hira: "にゃ",
+    kana: "ニャ",
+    romaji: "nya",
+  },
+  {
+    hira: "にゅ",
+    kana: "ニュ",
+    romaji: "nyu",
+  },
+  {
+    hira: "にょ",
+    kana: "ニョ",
+    romaji: "nyo",
+  },
+  {
+    hira: "ひゃ",
+    kana: "ヒャ",
+    romaji: "hya",
+  },
+  {
+    hira: "ひゅ",
+    kana: "ヒュ",
+    romaji: "hyu",
+  },
+  {
+    hira: "ひょ",
+    kana: "ヒョ",
+    romaji: "hyo",
+  },
+  {
+    hira: "みゃ",
+    kana: "ミャ",
+    romaji: "mya",
+  },
+  {
+    hira: "みゅ",
+    kana: "ミュ",
+    romaji: "myu",
+  },
+  {
+    hira: "みょ",
+    kana: "ミョ",
+    romaji: "myo",
+  },
+  {
+    hira: "りゃ",
+    kana: "リャ",
+    romaji: "rya",
+  },
+  {
+    hira: "りゅ",
+    kana: "リュ",
+    romaji: "ryu",
+  },
+  {
+    hira: "りょ",
+    kana: "リョ",
+    romaji: "ryo",
+  },
+  {
+    hira: "ぎゃ",
+    kana: "ギャ",
+    romaji: "gya",
+  },
+  {
+    hira: "ぎゅ",
+    kana: "ギュ",
+    romaji: "gyu",
+  },
+  {
+    hira: "ぎょ",
+    kana: "ギョ",
+    romaji: "gyo",
+  },
+  {
+    hira: "じゃ",
+    kana: "ジャ",
+    romaji: "ja",
+  },
+  {
+    hira: "じゅ",
+    kana: "ジュ",
+    romaji: "ju",
+  },
+  {
+    hira: "じょ",
+    kana: "ジョ",
+    romaji: "jo",
+  },
+  {
+    hira: "びゃ",
+    kana: "ビャ",
+    romaji: "bya",
+  },
+  {
+    hira: "びゅ",
+    kana: "ビュ",
+    romaji: "byu",
+  },
+  {
+    hira: "びょ",
+    kana: "ビョ",
+    romaji: "byo",
+  },
+  {
+    hira: "ぴゃ",
+    kana: "ピャ",
+    romaji: "pya",
+  },
+  {
+    hira: "ぴゅ",
+    kana: "ピュ",
+    romaji: "pyu",
+  },
+  {
+    hira: "ぴょ",
+    kana: "ピョ",
+    romaji: "pyo",
+  },
+];
+
+export enum OtoType {
+  seion = "seion",
+  dakuon = "dakuon",
+  youon = "youon",
+}
+
+export type Oto = {
+  // 音 おと
+  hira: string;
+  kana: string;
+  romaji: string;
+};
+
+export const HIRA_TO_OTO_TYPE = (function () {
+  let result: Record<string, OtoType> = {};
+  for (let i = 0; i < SEION_LIST.length; i++) {
+    result[SEION_LIST[i].hira] = OtoType.seion;
+  }
+  for (let i = 0; i < DAKUON_LIST.length; i++) {
+    result[DAKUON_LIST[i].hira] = OtoType.dakuon;
+  }
+  for (let i = 0; i < YOUON_LIST.length; i++) {
+    result[YOUON_LIST[i].hira] = OtoType.youon;
+  }
+  return result;
+})();
+
+export const HIRA_TO_OTO = (function () {
+  let result: Record<string, Oto> = {};
+  for (let i = 0; i < SEION_LIST.length; i++) {
+    result[SEION_LIST[i].hira] = SEION_LIST[i];
+  }
+  for (let i = 0; i < DAKUON_LIST.length; i++) {
+    result[DAKUON_LIST[i].hira] = DAKUON_LIST[i];
+  }
+  for (let i = 0; i < YOUON_LIST.length; i++) {
+    result[YOUON_LIST[i].hira] = YOUON_LIST[i];
+  }
+  return result;
+})();
---
+++ src/utils/index.ts
@@ -0,0 +1,98 @@
+import { GLOBAL_CONFIG } from "./config";
+import {
+  DAKUON_LIST,
+  HIRA_TO_OTO,
+  HIRA_TO_OTO_TYPE,
+  OtoType,
+  SEION_LIST,
+  YOUON_LIST,
+  type Oto,
+} from "./gojuon";
+
+export type Blank = {
+  fill: Oto;
+  answer: Oto;
+  blank: "hira" | "kana" | "romaji";
+};
+
+function searchOtoByHira(hira: string): Oto | undefined {
+  return HIRA_TO_OTO[hira];
+}
+
+function getHiraType(hira: string): OtoType | undefined {
+  return HIRA_TO_OTO_TYPE[hira];
+}
+
+function isOtoTypeEnabled(type: OtoType): boolean {
+  return (
+    (type === OtoType.seion && GLOBAL_CONFIG.enableSeion) ||
+    (type === OtoType.dakuon && GLOBAL_CONFIG.enableDakuon) ||
+    (type === OtoType.youon && GLOBAL_CONFIG.enableYouon)
+  );
+}
+
+function getFullList() {
+  let list: Oto[] = [];
+  if (GLOBAL_CONFIG.enableSeion) {
+    list = list.concat(SEION_LIST);
+  }
+  if (GLOBAL_CONFIG.enableDakuon) {
+    list = list.concat(DAKUON_LIST);
+  }
+  if (GLOBAL_CONFIG.enableYouon) {
+    list = list.concat(YOUON_LIST);
+  }
+  return list;
+}
+
+function getWeightedList() {
+  let result = getFullList();
+  Object.keys(GLOBAL_CONFIG.weights).forEach((k) => {
+    let weight = GLOBAL_CONFIG.weights[k];
+    const type = getHiraType(k);
+    if (type && weight > 0 && isOtoTypeEnabled(type)) {
+      let oto = searchOtoByHira(k);
+      if (oto) {
+        while (weight-- > 0) {
+          result.push(oto);
+        }
+      }
+    }
+  });
+  return result;
+}
+
+function getRandomBlankType() {
+  const types = [];
+  if (GLOBAL_CONFIG.blankHira) {
+    types.push("hira");
+  }
+  if (GLOBAL_CONFIG.blankKana) {
+    types.push("kana");
+  }
+  // if (GLOBAL_CONFIG.enableRomaji && GLOBAL_CONFIG.blankRomaji) {
+  //   types.push("romaji");
+  // }
+  return types[Math.floor(Math.random() * types.length)];
+}
+
+export function generateBlank() {
+  let list = getWeightedList();
+  let answer = list[Math.floor(Math.random() * list.length)];
+  let blank = getRandomBlankType();
+  let fill = answer;
+  (fill as { [key: string]: string })[blank] = "";
+  return { fill, answer, blank };
+}
+
+export function stringifyDuration(duration: number) {
+  let result = "";
+  duration /= 1000;
+  let minutes = Math.floor(duration / 60);
+  let seconds = Math.floor(duration % 60);
+  if (minutes > 0) {
+    result += minutes + "m";
+  }
+  result += seconds + "s";
+  return result;
+}
