fc6f8895eb4e0dd12be4c3d40c5b7ea671b8449e
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-02-04 11:25:09 +0800

    feat: add title & meta

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,10 @@
 <template>
+
+  <Head>
+    <title>Crazy Gojuon</title>
+    <meta name="description" content="Crazy Gojuon 50音训练" />
+    <meta name="keywords" content="japanese learning,gojuon" />
+  </Head>
   <div>
     <NuxtPage />
   </div>
