ee7c1fa4882e15d18c88adb0e7ce144e38f0a251
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-02-17 02:05:38 +0800

    fix: fix ring for disabled blanks

--- src/components/Blank.vue
+++ src/components/Blank.vue
@@ -1,7 +1,7 @@
 <template>
   <UInput :modelValue="value" :disabled="disabled" size="xl" color="white" @keydown.enter="$emit('submit')"
     @update:modelValue="value = $event"
-    :ui="{ wrapper: 'inline-block', base: 'size-16 select-none disabled:opacity-100 text-center', padding: { xl: 'p-0.5' }, size: { xl: 'text-[1em]' }, color: { white: { outline: (disabled ? 'ring-0' : 'ring-2') + ' ' + outline + ' ' + 'focus:ring-0 dark:focus:ring-0' } } }" />
+    :ui="{ wrapper: 'inline-block', base: 'size-16 select-none disabled:opacity-100 text-center', padding: { xl: 'p-0.5' }, size: { xl: 'text-[1em]' }, color: { white: { outline: (disabled ? '' : 'ring-2') + ' ' + outline + ' ' + 'focus:ring-0 dark:focus:ring-0' } } }" />
 </template>
 
 <script lang="ts" setup>
